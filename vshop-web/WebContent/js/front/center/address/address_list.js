04a8204f408e0c65339f0d1a9d2110efd3e1498b
'mldn' <'mldnqa@163.com'>
2017-05-02 11:01:35 +0800

    实现用户默认地址的变更操作

--- vshop-web/WebContent/js/front/center/address/address_list.js
+++ vshop-web/WebContent/js/front/center/address/address_list.js
@@ -2,7 +2,10 @@ $(function() {
 	$("#selectAll").on("click",function(){
 		checkboxSelectAll('aid',this.checked) ;
 	}) ;
-	$(defBtn).on("click",function(){	// 绑定用户锁定操作  
-		operateAlert(false,"默认配送地址修改成功！","默认配送地址修改失败！") ;
+	$(defBtn).on("click",function(){	// 绑定用户锁定操作
+		adid = $("#adid:checked").val() ;	// 取得要设置为当前默认地址的编号
+		$.post("pages/front/center/address/MemberAddressActionFront!editDeflag.action", {"adid":adid},function(data){
+			operateAlert(data.trim() == "true","默认配送地址修改成功！","默认配送地址修改失败！") ;
+		},"text") ;
 	}) ;
 })
