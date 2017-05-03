87b25130cdae3102816c9e5b2f80559e97c29b9a
'mldn' <'mldnqa@163.com'>
2017-05-03 10:12:19 +0800

    实现网站前台商品分类信息的显示。

--- vshop-web/WebContent/js/index.js
+++ vshop-web/WebContent/js/index.js
@@ -1,4 +1,30 @@
 $(function() {
+	
+	$.get("item.json",{},function(data){
+		for (x = 0 ; x < data.items.length ; x ++){
+			itemData = 
+				"<div class='panel panel-primary'>" +
+				"		<div class='panel-heading'>" + 
+				"			<h4 class='panel-title'>" +
+				"				<a data-toggle='collapse' data-parent='#item' href='#content-"+data.items[x].item.iid+"'>" + 
+				"					" + data.items[x].item.title +
+				"				</a>" +
+				"			</h4>" +
+				"		</div>" +
+				"		<div id='content-"+data.items[x].item.iid+"' class='panel-collapse collapse "+(x == 0 ? "in" : "")+"'>" +
+				"			<div class='panel-body'>" +
+				"				<div class='row'>"  ;
+			for (y = 0 ; y < data.items[x].subitems.length ; y ++) {
+				itemData += "<div class='col-md-4'><a href='#'>"+data.items[x].subitems[y].title+"</a></div>" ;
+			}
+			itemData += "				</div>" +
+						"			</div>" +
+						"		</div>" +
+						"	</div>" ;
+			$(item).append(itemData) ;
+		}
+	},"json") ;
+	
 	$("button[id*=addCar-]").each(function(){
 		var gid = $(this).attr("id").split("-")[1] ;
 		$(this).on("click",function(){
